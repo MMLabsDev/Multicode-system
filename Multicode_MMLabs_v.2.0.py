c6fb94f4ed9e63baf833dddb2f778007f6a9e1be
Miguel Martínez <294660817+MMLabsDev@users.noreply.github.com>
2026-07-14 11:23:00 -0600

    Multicode.M.MLabs.py V.2.1
    Adición del lenguaje base64 y mejoras en el chatbot.

--- Multicode_MMLabs_v.2.0.py
+++ Multicode_MMLabs_v.2.0.py
@@ -1,4 +1,6 @@
 #importar librerías........................................................................................................................... 
+import base64
+import time
 import random
 import os
 import pyperclip
@@ -14,11 +16,12 @@ tabla.add_column("Herramienta")
 
 tabla.add_row("  [1]","ASCII")
 tabla.add_row("  [2]","Binario")
-tabla.add_row("  [3]","Hexadecimal")
-tabla.add_row("  [4]","Unicode")
-tabla.add_row("  [5]","Detector de código(Beta)")
-tabla.add_row("  [6]","Chatbot")
-tabla.add_row("  [7]","Cerrar Programa")
+tabla.add_row("  [3]","Base64")
+tabla.add_row("  [4]","Hexadecimal")
+tabla.add_row("  [5]","Unicode")
+tabla.add_row("  [6]","Detector de código(Beta)")
+tabla.add_row("  [7]","Chatbot")
+tabla.add_row("  [8]","Cerrar Programa")
 
 #tabla ASCII---------------------------------------------------------------
 
@@ -53,6 +56,14 @@ tabla4.add_column("Traductor")
 tabla4.add_row("  [1]","Epañol - Unicode")
 tabla4.add_row("  [2]","Unicode - Español")
 tabla4.add_row("  [3]","Regresar")
+#Tabla base64---------------------------------------------------------------
+tabla6 = Table(title=""" Selecione el tipo de traducción""")
+tabla6.add_column("Función")
+tabla6.add_column("Traductor")
+
+tabla6.add_row("  [1]","Español - Base64")
+tabla6.add_row("  [2]","Base64 - Español")
+tabla6.add_row("  [2]","Regresar")
 #tabla Chatbot--------------------------------------------------------------
 tabla5 = Table(title="[bold lightblue]Mia[/bold lightblue]")
 tabla5.add_column("[italic]Multicode Intelligent Assistant[/italic]")
@@ -123,6 +134,22 @@ def hexadecimal_a_palabra():
 
     print("Traducción: ", palabra)
     return palabra
+
+def palabra_a_base64():
+    texto = input("Texto: ")
+
+    codificado = base64.b64encode(texto.encode()).decode()
+
+    print(codificado)
+    return codificado
+
+def base64_a_palabra():
+    texto = input("Base64: ")
+
+    original = base64.b64decode(texto).decode()
+
+    print(original)
+    return original
 
 def palabra_a_unicode():
     texto = input("Texto: ")
@@ -247,8 +274,41 @@ while True:
                     console.print(tabla)
                     break
                 
-            
+
         elif seleccion == 3:
+            console.print(tabla6)
+
+            while True:
+                funcion = int(input("#: "))
+
+                if funcion == 1:
+                    codificado = palabra_a_base64()
+                    option = input("\n¿Copiar? (s/n): ")
+
+                    if option.lower() == "s":
+                        pyperclip.copy(codificado)
+                        console.print(":white_check_mark: Copiado.")
+
+                    elif option.lower() == "n":
+                        continue
+
+                elif funcion == 2:
+                    original = base64_a_palabra()
+                    option = input("\n¿Copiar? (s/n): ")
+
+                    if option.lower() == "s":
+                        pyperclip.copy(original)
+                        console.print(":white_check_mark: Copiado.")
+
+                    elif option.lower() == "n":
+                        continue
+
+                elif funcion == 3:
+                    limpiar()
+                    console.print(tabla)
+                    break
+                    
+        elif seleccion == 4:
             console.print(tabla3)
 
             while True:
@@ -269,7 +329,7 @@ while True:
                     traduccion = hexadecimal_a_palabra()
                     option = input("\n¿Copiar? (s/n): ")
 
-                    if option.lower == "s":
+                    if option.lower() == "s":
                         pyperclip.copy(traduccion)
                         console.print(":white_check_mark: Copiado.")
 
@@ -283,7 +343,7 @@ while True:
                     break
 
 
-        elif seleccion == 4:
+        elif seleccion == 5:
             console.print(tabla4)
 
             while True:
@@ -319,7 +379,7 @@ while True:
                     break
 
            
-        elif seleccion == 5:
+        elif seleccion == 6:
             while True:
                 print("\n Escribe exit<> para salir del modo detector")
                 texto = input("Ingrese el código: ")
@@ -329,21 +389,52 @@ while True:
                 if texto.lower() == "exit<>":
                     break
 
-        elif seleccion == 6:
+        elif seleccion == 7:
             console.print(tabla5)
+            print("Así que dime...")
+            
             while True:
-                usuario = input("Así que dime...En qué te puedo ayudar hoy?")
+                usuario = input("En qué te puedo ayudar?")
+                
+                
+                if "adiós" in usuario.lower() or "adios" in usuario.lower():
+                    user = input("Adiós usuario, ¿deseas salir? [s/n]")
 
-                if usuario.lower() == "salir":
-                    break
+                    if user.lower() == "n" or "no":
+                        print("No hay problema")
+                        usuario = input("¿Qué sigue?")
+
+                    elif user.lower() == "s" or "si":
+                        print("Okey, adiós usuario")
+                        time.sleep(1)
+                        break
+
+                elif "hola" in usuario.lower():
+                    print("¡Hola usuario!, sigo aquí, dime...¿qué necesitas?")
+
+                elif "gracias" in usuario.lower() or "muchas gracias" in usuario.lower():
+                    print("Un gusto :)")
+
+                elif "qué puedes hacer" in usuario.lower() or "que puedes hacer" in usuario.lower():
+                    print("Puedo detectar el código que me digas, traducir yo misma lo que quieras y te puedo asesorar aquí en Multicode-System ;)")
+
+                elif "como te llamas" in usuario.lower() or "cómo te llamas" in usuario.lower() or "cuál es tu nombre" in usuario.lower() or "cual es tu nombre" in usuario.lower():
+                    print("Me llamo Mia, soy la abreviación de 'Multicode intelligent asistant'")
+
                 elif usuario.lower() == "ascii":
                     print("Sí, sé hablar ASCII también, dime, qué quieres traducir?")
+
                 elif usuario.lower() == "binario":
                     print("Sí, sé hablar Binario también, dime, qué quieres traducir?")
+
+                elif usuario.lower() == "salir":
+                    console.print("[bold red]Cerrando chatbot[/bold red]")
+                    break
+
                 else:
                     print("ehh...lo siento, no entiendo eso aún")
 
-        elif seleccion == 7:
+        elif seleccion == 8:
             break
 
         else:
